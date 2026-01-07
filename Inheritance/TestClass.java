012fcb4edb20f7fd4e50ad662180ac4a73c161be
Chandni-53 <rajawatchandni53@gmail.com>
2026-01-07 19:34:18 +0530

    new file is added

---
+++ Inheritance/TestClass.java
@@ -0,0 +1,25 @@
+package Inheritance;
+
+public class TestClass extends Student{
+
+    TestClass(String name, int age, String gender) {
+        super(name, age, gender);
+        //TODO Auto-generated constructor stub
+    }
+    TestClass(){
+        super("unknown",0,"unknown");
+    }//default
+    @Override
+    public String toString() {
+        return "Hello, I'm "+name;
+    }
+    public static void main(String[] args) {
+        TestClass t1=new TestClass();
+        TestClass t2=new TestClass("Chandni",21,"female");
+        System.out.println(t1.toString());
+        System.out.println(t2.toString());
+        System.out.println(t1.getClass());
+        System.out.println(t1.hashCode());
+    }
+    
+}
