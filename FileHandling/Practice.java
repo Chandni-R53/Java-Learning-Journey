fdfedde3c755dc81014b47d7c12bd7b9fe107f14
Chandni-53 <rajawatchandni53@gmail.com>
2026-01-22 00:03:13 +0530

    practicce file added

---
+++ FileHandling/Practice.java
@@ -0,0 +1,26 @@
+package FileHandling;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+public class Practice {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the file name: ");
+        String fileName=sc.nextLine();
+        try(FileReader reader=new FileReader(fileName)){
+            int read=0;
+            do{
+                read=reader.read();
+                System.out.print((char)read);
+            }while(read!=-1);
+
+        }catch(FileNotFoundException e){
+           System.out.printf("%s file not found",fileName);
+        }catch(IOException e){
+             System.out.printf("Exception occurred: %s",e.getMessage());
+        }
+
+
+    }
+}
